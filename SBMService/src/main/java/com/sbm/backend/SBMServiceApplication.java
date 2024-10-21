272319493e6d9d8731e5705a3103b1ceb954988b
goraicorporation <goraicorporation@gmail.com>
2024-10-21 20:08:40 +0530

    some file/folder name changes

--- SBMService/src/main/java/com/sbm/backend/SBMServiceApplication.java
+++ SBMService/src/main/java/com/sbm/backend/SBMServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Eco2MServiceApplication {
+public class SBMServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Eco2MServiceApplication.class, args);
+		SpringApplication.run(SBMServiceApplication.class, args);
 	}
 
 }
